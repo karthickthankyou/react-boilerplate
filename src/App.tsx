2be6a3ce390dfca9a98d4e3852739ee56d546c7f
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-06-04 13:42:23 +0530

    feat(app.tsx): did a crucial modification

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div className="flex p-6 m-4 text-xs text-red-600 bg-yellow-400">
-      Hello World
+      Hello World!
     </div>
   )
 }
